a7d3f5ac0628de00ebcf528be9de3afa618a1937
Nitin Rai <nitinrai.dev@gmail.com>
2022-10-08 04:05:18 +0530

    Update ToDo.js

--- src/components/TODO Section/ToDo.js
+++ src/components/TODO Section/ToDo.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { BiCircle, BiDotsVerticalRounded } from "react-icons/bi";
 import { v4 as uuidv4 } from "uuid";
 import { Flex } from "../Styles/Flexbox";
 
 const ToDo = () => {
-  const initialState = JSON.parse(localStorage.getItem('todos')) || [];
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [showInput, setShowInput] = useState(false);
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState(initialState);
   const [editTodo, setEditTodo] = useState(null);
+  const [dropdown, setDropdown] = useState(null);
 
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
   const handleToggle = () => {
@@ -23,14 +25,14 @@ const ToDo = () => {
       todo.id === id ? { title, id, completed } : todo
     );
     setTodos(newTodo);
-    setEditTodo('');
+    setEditTodo("");
   };
 
   useEffect(() => {
     if (editTodo) {
       setInput(editTodo.title);
     } else {
-      setInput('');
+      setInput("");
     }
   }, [setInput, editTodo]);
 
@@ -39,41 +41,46 @@ const ToDo = () => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(!editTodo) {
+    if (!editTodo) {
       setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
       setInput("");
     } else {
-      updateTodo(input, editTodo.id, editTodo.completed)
+      updateTodo(input, editTodo.id, editTodo.completed);
     }
   };
 
   const handleComplete = (todo) => {
     setTodos(
       todos.map((item) => {
-        if(item.id === todo.id) {
-          return {...item, completed: !item.completed}
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
         }
         return item;
       })
-    )
+    );
   };
-  const handleEdit = ({id}) => {
+  const handleEdit = ({ id }) => {
     setShowInput(true);
     const findTodo = todos.find((todo) => todo.id === id);
     setEditTodo(findTodo);
   };
-  const handleDelete = ({id}) => {
+  const handleDelete = ({ id }) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleAction = ({ id }) => {
+    setDropdown((prev) => {
+      return prev === id ? null : id;
+  });
+  }
 
   return (
     <StyledToDo>
       <h4>
-        <Flex spaceBetween>ToDo List 
-
-        <button className={editTodo ? 'disabled' : ''} onClick={handleToggle}>
-          {showInput ? "Cancel" : "Add a task"}
+        <Flex spaceBetween>
+          ToDo List
+          <button className={editTodo ? "disabled" : ""} onClick={handleToggle}>
+            {showInput ? "Cancel" : "Add a task"}
           </button>
         </Flex>
       </h4>
@@ -106,13 +113,22 @@ const ToDo = () => {
         <div className="eachItem">
           {todos.map((todo) => {
             return (
-              <Flex className={`${todo.completed ? 'completed' : ''}`} spaceBetween key={todo.id}>
-                {todo.title}  
-                <div>
-                  <button className="hidden" onClick={() => handleComplete(todo)}>Done</button>
-                  <button onClick={() => handleEdit(todo)}>Edit</button>
-                  <button onClick={() => handleDelete(todo)}>Delete</button>
-                </div>
+              <Flex
+                spaceBetween
+                gap="10px"
+                key={todo.id}
+              >
+                <button className={`taskText ${todo.completed ? "completed" : ""}`} onClick={() => handleComplete(todo)} title="Done">
+                  <BiCircle />
+                  <span>{todo.title}</span>
+                </button>
+                <button onClick={() => handleAction(todo)}>
+                  <BiDotsVerticalRounded  title="More" />
+                  <div className={`moreAction ${dropdown === todo.id ? 'active' : ''}`}>
+                    <span onClick={() => handleEdit(todo)}>Make changes</span>
+                    <span onClick={() => handleDelete(todo)}>Delete this task</span>
+                  </div>
+                </button>
               </Flex>
             );
           })}
@@ -171,11 +187,57 @@ const StyledToDo = styled.div`
     & .eachItem > div {
       background: transparent;
       margin-block: 0.5rem;
-      padding: 12px 10px;
       border-radius: 6px;
       border: 1px solid #ddd;
-      & button.hidden {
+      & > button {
+        position: relative;
+        background: transparent;
+        border: 0;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        padding: 12px 10px;
+        text-align: left;
+        & svg {
+          min-width: 20px;
+          width: 20px;
+          height: 20px;
+          border-radius: 100%;
+          transition: transform 0.3s ease-in-out;
+        }
+        &.completed svg {
+          background: #111;
+          transform: scale(0.8);
+        }
+        &.taskText {
+          flex: 1;
+        }
+        &.completed span {
+          text-decoration: line-through;
+          color: #777;
+        }
+      }
+      & .moreAction {
         display: none;
+        position: absolute;
+        top: 90%;
+        background: #fff;
+        border: 1px solid #ddd;
+        padding: 10px;
+        right: -10px;
+        border-radius: 4px;
+        z-index: 2;
+        box-shadow: 0 1px 2px 0 rgba(60,64,67,0.302),0 2px 6px 2px rgba(60,64,67,0.149);
+        width: max-content;
+        &.active {
+          display: block;
+        }
+        & span {
+          display: block;
+          font-size: 14px;
+          line-height: 1.8;
+          letter-spacing: 0.02em;
+        }
       }
     }
   }
